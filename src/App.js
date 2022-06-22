86c5de86f41455a0e9c55446fc0e2c76300adf52
FrederickDavid <muomaifefrederick61@gmail.com>
2022-06-22 16:03:25 -0700

    Build Added

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import DisplayPidgin from "./Frederick/DisplayPidgin";
+
+function App() {
+  return (
+    <div className="App">
+      <DisplayPidgin />
+    </div>
+  );
+}
+
+export default App;
